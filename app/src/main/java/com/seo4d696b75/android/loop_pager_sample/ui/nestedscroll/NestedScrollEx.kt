d07ef2cce73e96ad45498d45ed6f83a366b54d84
Seo-4d696b75 <s.kaoru509@gmail.com>
2024-12-25 23:50:21 +0900

    feat: NestedScroll extension added

---
+++ app/src/main/java/com/seo4d696b75/android/loop_pager_sample/ui/nestedscroll/NestedScrollEx.kt
@@ -0,0 +1,37 @@
+package com.seo4d696b75.android.loop_pager_sample.ui.nestedscroll
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
+import androidx.compose.ui.input.nestedscroll.NestedScrollSource
+
+/**
+ * Gets an adapter of this [NestedScrollConnection]
+ * which will only consume scroll from [NestedScrollSource.UserInput].
+ *
+ * This is useful for a case in which both collapsing `TopAppBar` and `VerticalPager` are placed,
+ * and scroll amount dispatched from snap animations of the pager should NOT be consumed.
+ */
+@Composable
+fun NestedScrollConnection.filterUserInput() = remember(this) {
+    val original = this
+    object : NestedScrollConnection {
+        override fun onPreScroll(available: Offset, source: NestedScrollSource) =
+            if (source == NestedScrollSource.UserInput) {
+                original.onPreScroll(available, source)
+            } else {
+                Offset.Zero
+            }
+
+        override fun onPostScroll(
+            consumed: Offset,
+            available: Offset,
+            source: NestedScrollSource,
+        ): Offset = if (source == NestedScrollSource.UserInput) {
+            original.onPostScroll(consumed, available, source)
+        } else {
+            Offset.Zero
+        }
+    }
+}
