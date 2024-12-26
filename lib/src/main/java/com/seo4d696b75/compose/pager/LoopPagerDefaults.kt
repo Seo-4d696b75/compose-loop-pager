425a988086b951dc9da24b930bd3bb6fa8f1b706
Seo-4d696b75 <s.kaoru509@gmail.com>
2024-12-27 00:38:51 +0900

    fix: fling animation logic

--- lib/src/main/java/com/seo4d696b75/compose/pager/LoopPagerDefaults.kt
+++ lib/src/main/java/com/seo4d696b75/compose/pager/LoopPagerDefaults.kt
@@ -2,15 +2,17 @@ package com.seo4d696b75.compose.pager
 
 import androidx.compose.animation.core.AnimationSpec
 import androidx.compose.animation.core.DecayAnimationSpec
-import androidx.compose.animation.core.exponentialDecay
 import androidx.compose.animation.core.spring
+import androidx.compose.animation.rememberSplineBasedDecay
 import androidx.compose.foundation.gestures.TargetedFlingBehavior
 import androidx.compose.foundation.gestures.snapping.SnapLayoutInfoProvider
 import androidx.compose.foundation.gestures.snapping.snapFlingBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.remember
+import kotlin.math.absoluteValue
 import kotlin.math.roundToInt
+import kotlin.math.sign
 
 @Stable
 object LoopPagerDefaults {
@@ -18,7 +20,7 @@ object LoopPagerDefaults {
     fun flingBehavior(
         state: LoopPagerState,
         snapAnimationSpec: AnimationSpec<Float> = spring(),
-        decayAnimationSpec: DecayAnimationSpec<Float> = exponentialDecay(),
+        decayAnimationSpec: DecayAnimationSpec<Float> = rememberSplineBasedDecay(),
     ): TargetedFlingBehavior = remember(state, snapAnimationSpec, decayAnimationSpec) {
         snapFlingBehavior(
             snapLayoutInfoProvider = LoopPagerSnapLayoutProvider(state),
@@ -51,8 +53,16 @@ internal class LoopPagerSnapLayoutProvider(
                 val currentPage = state.page
                 val decayPage = currentPage - decayOffset / info.pageInterval
                 val snapPage = decayPage.roundToInt()
-                state.targetPage = snapPage
-                -(snapPage - currentPage) * info.pageInterval
+                val distance = (snapPage - currentPage).absoluteValue
+
+                // We'd like the approach animation to finish right before the last page so we can
+                // use a snapping animation for the rest.
+                return if (distance <= 1f) {
+                    0f
+                } else {
+                    state.targetPage = snapPage
+                    distance * info.pageInterval * velocity.sign
+                }
             }
 
             LoopPagerLayoutInfo.Zero -> 0f
