34dea134ee3cbe86b04a63fe1b4f07fcd02241ce
Seo-4d696b75 <s.kaoru509@gmail.com>
2024-12-30 13:41:37 +0900

    feat: publish task added

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,6 +1,11 @@
+import kotlin.io.encoding.Base64
+import kotlin.io.encoding.ExperimentalEncodingApi
+
 plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("maven-publish")
+    id("signing")
 }
 
 android {
@@ -43,6 +48,12 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.14"
     }
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
 }
 
 dependencies {
@@ -56,4 +67,87 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            register<MavenPublication>("maven") {
+                groupId = "com.seo4d696b75.compose"
+                artifactId = "loop-pager"
+                version = libs.versions.loopPager.get()
+                from(components["release"])
+
+                pom {
+                    name = "Compose Loop Pager"
+                    description = "Android Jetpack Compose loop pager scrollable infinitely"
+                    url = "https://github.com/Seo-4d696b75/compose-loop-pager"
+                    licenses {
+                        license {
+                            name = "MIT License"
+                            url = "https://opensource.org/license/mit"
+                            distribution = "repo"
+                        }
+                    }
+                    developers {
+                        developer {
+                            id = "seo4d696b75"
+                            name = "Seo-4d696b75"
+                            email = "s.kaoru509@gmail.com"
+                        }
+                    }
+                    scm {
+                        connection =
+                            "scm:git:https://github.com/Seo-4d696b75/compose-loop-pager.git"
+                        developerConnection =
+                            "scm:git:ssh://github.com/Seo-4d696b75/compose-loop-pager.git"
+                        url = "https://github.com/Seo-4d696b75/compose-loop-pager"
+                    }
+                }
+            }
+        }
+        repositories {
+            maven {
+                // maven-publish plugin is not used for publishing,
+                // but only for generating artifact files
+                name = "tmp"
+                url = uri(layout.buildDirectory.dir("tmp"))
+            }
+        }
+    }
+    signing {
+        sign(publishing.publications["maven"])
+    }
+}
+
+// zip all the files to be published before curl POST
+tasks.register<Zip>("archivePublication") {
+    dependsOn("publishMavenPublicationToTmpRepository")
+    from(layout.buildDirectory.dir("tmp/com/seo4d696b75/compose/loop-pager/${libs.versions.loopPager.get()}"))
+    into("/com/seo4d696b75/compose/loop-pager/${libs.versions.loopPager.get()}")
+}
+
+// TODO find official gradle plugin
+// publish via Central Publisher API
+// @see https://central.sonatype.com/api-doc
+tasks.register("publishMavenCentralAPI") {
+    dependsOn("archivePublication")
+    doLast {
+        val username = project.properties["SONATYPE_USERNAME"].toString()
+        val password = project.properties["SONATYPE_PASSWORD"].toString()
+
+        @OptIn(ExperimentalEncodingApi::class)
+        val auth = Base64.encode("$username:$password".encodeToByteArray())
+
+        exec {
+            commandLine = listOf(
+                "curl",
+                "--request", "POST",
+                "--verbose",
+                "--header", "Authorization: Bearer $auth",
+                "--form", "bundle=@build/distributions/lib.zip",
+                "https://central.sonatype.com/api/v1/publisher/upload?publishingType=AUTOMATIC",
+            )
+        }
+    }
 }
