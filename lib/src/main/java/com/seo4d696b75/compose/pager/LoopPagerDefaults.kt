1a734c59648f1e116947b3c6f0eab0bdf2656672
Seo-4d696b75 <s.kaoru509@gmail.com>
2025-01-15 18:18:45 +0900

    fix: unable to fling to a neighbor page

--- lib/src/main/java/com/seo4d696b75/compose/pager/LoopPagerDefaults.kt
+++ lib/src/main/java/com/seo4d696b75/compose/pager/LoopPagerDefaults.kt
@@ -10,7 +10,12 @@ import androidx.compose.foundation.gestures.snapping.snapFlingBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.dp
 import kotlin.math.absoluteValue
+import kotlin.math.ceil
+import kotlin.math.floor
 import kotlin.math.roundToInt
 import kotlin.math.sign
 
@@ -21,23 +26,39 @@ object LoopPagerDefaults {
         state: LoopPagerState,
         snapAnimationSpec: AnimationSpec<Float> = spring(),
         decayAnimationSpec: DecayAnimationSpec<Float> = rememberSplineBasedDecay(),
-    ): TargetedFlingBehavior = remember(state, snapAnimationSpec, decayAnimationSpec) {
-        snapFlingBehavior(
-            snapLayoutInfoProvider = LoopPagerSnapLayoutProvider(state),
-            decayAnimationSpec = decayAnimationSpec,
-            snapAnimationSpec = snapAnimationSpec,
-        )
+        velocityThreshold: Density.() -> Float = { 400.dp.toPx() },
+    ): TargetedFlingBehavior {
+        val density = LocalDensity.current
+        val velocityThresholdPx = with(density) { velocityThreshold() }
+        return remember(state, snapAnimationSpec, decayAnimationSpec, velocityThresholdPx) {
+            snapFlingBehavior(
+                snapLayoutInfoProvider = LoopPagerSnapLayoutProvider(state, velocityThresholdPx),
+                decayAnimationSpec = decayAnimationSpec,
+                snapAnimationSpec = snapAnimationSpec,
+            )
+        }
     }
 }
 
 internal class LoopPagerSnapLayoutProvider(
     private val state: LoopPagerState,
+    private val velocityThreshold: Float,
 ) : SnapLayoutInfoProvider {
     override fun calculateSnapOffset(velocity: Float): Float {
-        // snap to the closest page
-        val snapPage = state.currentPage
         return when (val info = state.layoutInfo) {
             is LoopPagerLayoutInfo.Measured -> {
+                val currentPage = state.page
+                val snapPage = if (velocity.absoluteValue < velocityThreshold) {
+                    // snap to the closest page
+                    currentPage.roundToInt()
+                } else {
+                    // snap to the neighbor page in same direction as velocity
+                    if (velocity > 0) {
+                        floor(currentPage).roundToInt()
+                    } else {
+                        ceil(currentPage).roundToInt()
+                    }
+                }
                 state.targetPage = snapPage
                 -(snapPage - state.page) * info.pageInterval
             }
@@ -48,6 +69,7 @@ internal class LoopPagerSnapLayoutProvider(
 
     override fun calculateApproachOffset(velocity: Float, decayOffset: Float): Float {
         // snap to the closed page position around the decayOffset
+        // Note: decay animation is only used when the actual scroll offset > pageInterval
         return when (val info = state.layoutInfo) {
             is LoopPagerLayoutInfo.Measured -> {
                 val currentPage = state.page
@@ -56,12 +78,14 @@ internal class LoopPagerSnapLayoutProvider(
                 val distance = (snapPage - currentPage).absoluteValue
 
                 // We'd like the approach animation to finish right before the last page so we can
-                // use a snapping animation for the rest.
-                return if (distance <= 1f) {
+                // use a snapping animation for the rest (<= pageInterval).
+                val decayDistance = (distance - 1f).coerceAtLeast(0f)
+
+                return if (decayDistance == 0f) {
                     0f
                 } else {
                     state.targetPage = snapPage
-                    distance * info.pageInterval * velocity.sign
+                    decayDistance * info.pageInterval * velocity.sign
                 }
             }
 
